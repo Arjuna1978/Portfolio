37c7dc970e6419eb6ecfd3491dec0e9efb01856f
Arjuna1978 <vijayanayagam@gmail.com>
2025-08-25 13:42:05 +1000

    routing fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from '@astrojs/react';
 
+// Get the public base URL from an environment variable,
+// or use an empty string as the default for local development.
+// This makes the configuration dynamic for different deployment environments.
 const PUBLIC_BASE_URL = process.env.PUBLIC_BASE_URL || '';
 
 // https://astro.build/config
@@ -10,7 +13,7 @@ export default defineConfig({
   // The full URL of your deployed site
   site: 'https://arjuna1978.github.io',
   
-  // The base path of your site on GitHub Pages
+  // The base path of your site on GitHub Pages.
   // This must match your repository name and is used as the prefix for all internal links.
   base: PUBLIC_BASE_URL,
 
